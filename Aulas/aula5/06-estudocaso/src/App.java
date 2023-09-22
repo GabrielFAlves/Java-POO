a7722adeafe5882de8097f20e3cdcebe08f8eae2
GABRIEL FIRMAMENTO ALVES <202302858112@alunos.ibmec.edu.br>
2023-09-22 07:45:06 -0300

    pac

--- Aulas/aula5/06-estudocaso/src/App.java
+++ Aulas/aula5/06-estudocaso/src/App.java
@@ -6,8 +6,8 @@ import entidades.pessoas.Paciente;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Paciente pc = new Paciente("João", "123", new Endereco(), LocalDate.of(2000, 8, 10));
-        Medico med = new Medico("Ana", "456", new Endereco(), LocalDate.of(1980, 5, 11));
+        Paciente pac = new Paciente("João", "123", new Endereco(), LocalDate.of(2000, 8, 10));
+        Medico med = new Medico("Ana", "456", new Endereco(), LocalDate.of(1980, 5, 11), "12665128");
 
         System.out.println(pac);
         System.out.println(med);
